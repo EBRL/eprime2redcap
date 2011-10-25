688afb71c4a4e60c9b8db4db837d81752a3e5908
Scott Burns <scott.s.burns@gmail.com>
2011-10-25 09:55:55 -0500

    Add basic redcap upload functionality

---
+++ rc_upload.py
@@ -0,0 +1,35 @@
+import os
+from ConfigParser import ConfigParser
+
+import redcap
+
+
+cfg = ConfigParser()
+cfg.read([os.path.expanduser('~/pycap.cfg')])
+KEY = config.get('keys', 'In-Magnet')
+
+del cfg
+
+URL = 'https://redcap.vanderbilt.edu/api/'
+
+project = redcap.Project(URL, KEY)
+
+def upload(to_upload):
+    """ Upload a dictionary to the In-Magnet database 
+    
+    Parameters
+    ----------
+    to_upload: dict
+        Dictionary of data to upload
+        
+    Returns
+    -------
+    success: bool
+        False on failure
+    """
+    data = [to_upload]
+    num_uploaded = project.import_records(data)
+    success = True
+    if num_uploaded != len(data):
+        success = False
+    return success
