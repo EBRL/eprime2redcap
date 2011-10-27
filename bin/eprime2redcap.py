129323dd440fb0f5d97137c42d02b729d36ab201
Scott Burns <scott.s.burns@gmail.com>
2011-10-27 16:57:38 -0500

    Add directory parsing to cli app

--- bin/eprime2redcap.py
+++ bin/eprime2redcap.py
@@ -3,6 +3,9 @@
 
 
 import os
+import re
+from glob import glob
+
 
 import ep2rc
 
@@ -13,7 +16,14 @@ def arguments():
     #  Arguments
     ap.add_argument('-f', dest='file',
         help='Parse and upload data from a (properly named) file')
+    ap.add_argument('-d', dest='dir',
+        help="Parse and upload all files from the given directory")
     return ap.parse_args()
+
+def parse_and_upload(fname):
+    with open(fname) as f:
+        to_redcap = ep2rc.parse_file(fname, f)
+    return ep2rc.upload(to_redcap)
 
 
 if __name__ == '__main__':
@@ -22,6 +32,23 @@ if __name__ == '__main__':
     if args.file:
         if not os.path.isfile(args.file):
             raise ValueError("This file doesn't exist")
-        with open(args.file) as f:
-            to_redcap =  ep2rc.parse_file(args.file, f)
-        ep2rc.upload(to_redcap)
+        parse_and_upload(fname)
+    if args.dir:
+        if not os.path.isdir(args.dir):
+            raise ValueError("This directory doesn't exist")
+        all_txt = glob(os.path.join(args.dir, '*.txt'))
+        files_to_parse = []
+        for txt in all_txt:
+            bname = os.path.basename(txt)
+            pattern = '(.*_){3,5}'
+            if re.match(pattern, bname):
+                try:
+                    if parse_and_upload(txt):
+                        msg = "Success with %s"
+                    else:
+                        msg = "Failed with %s"
+                except ep2rc.BadDataError:
+                    msg = "Failed with %s"
+                print msg % txt
+                    
+                    
