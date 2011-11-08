da3b47b6868fdad3442af6b6ec4fa2ff5ae03b42
Scott Burns <scott.s.burns@gmail.com>
2011-11-08 11:47:37 -0600

    Further commit for multiple databases

--- bin/eprime2redcap.py
+++ bin/eprime2redcap.py
@@ -18,16 +18,20 @@ def arguments():
         help='Parse and upload data from a (properly named) file')
     ap.add_argument('-d', dest='dir',
         help="Parse and upload all files from the given directory")
+    ap.add_argument('--database', dest='database', default='in-magnet',
+        help="Upload results to the given database")
+    ap.add_argument('--no-upload', dest='upload', default=True, action='store_false',
+        help='Do NOT upload information to redcap')
     return ap.parse_args()
 
-def parse_and_upload(fname):
+def parse_and_upload(fname, database, do_upload=True):
     to_redcap = {}
     with open(fname) as f:
         to_redcap = ep2rc.parse_file(fname, f)
     suc = False
-    if to_redcap:
-        suc = ep2rc.upload(to_redcap)
-    return suc
+    if to_redcap and do_upload:
+        suc = ep2rc.upload(to_redcap, database)
+    return to_redcap, suc
 
 if __name__ == '__main__':
     args = arguments()
@@ -35,7 +39,9 @@ if __name__ == '__main__':
     if args.file:
         if not os.path.isfile(args.file):
             raise ValueError("This file doesn't exist")
-        parse_and_upload(args.file)
+        data, success = parse_and_upload(args.file, args.database, args.upload)
+        if not args.upload:
+            print data
     if args.dir:
         if not os.path.isdir(args.dir):
             raise ValueError("This directory doesn't exist")
@@ -45,12 +51,12 @@ if __name__ == '__main__':
             pattern = '(.*_){3,5}'
             if re.match(pattern, bname):
                 try:
-                    if parse_and_upload(txt):
+                    data, success = parse_and_upload(txt, args.database, args.upload)
+                    if success:
                         msg = "Success with %s"
                     else:
                         msg = "Failed with %s"
                 except ep2rc.BadDataError:
                     msg = "(BadDataError) Failed with %s"
                 print msg % txt
-                    
-                    
+
