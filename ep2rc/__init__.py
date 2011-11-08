bc31383e4c2963dbbc3445760384bc0f3b0b8d39
Scott Burns <scott.s.burns@gmail.com>
2011-11-08 15:40:20 -0600

    Version bump

--- ep2rc/__init__.py
+++ ep2rc/__init__.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 __author__ = 'Scott Burns <scott.s.burns@gmail.com>'
 __license__ = 'BSD 3-Clause'
-__version__ = '0.1.1'
+__version__ = '0.3'
 
 from core import parse_file
 from rc import upload
